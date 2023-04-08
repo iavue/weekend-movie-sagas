a14e151c6ead2771bd618851071c2662157636a5
Ia Vue <iavue00@gmail.com>
2023-04-08 12:10:03 -0500

    used the && operator to check the details array for elements before rending its properties to the dom

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -2,30 +2,32 @@ import { useSelector } from 'react-redux';
 import React from 'react';
 
 function Details() {
-   
     const details = useSelector(store => store.genres);
 
     console.log('what are my details:', details);
 
     
-
+     
     return (
         <>
-        <h2>Movie Details View</h2>
+            <h2>Movie Details View</h2>
+            {/*  */}
+            {details.length &&
+                <>
+                    <img src={details[0].poster}/>
+                    <li>Title: {details[0].title}</li>
+                    <li>Description: {details[0].description}</li>
 
-        <img src={details[0].poster}/>
-        <li>Title: {details[0].title}</li>
-        <li>Description: {details[0].description}</li>
-
-        {details.map((detail) => (
-            <div key={detail.id}>
-                {/* <li>Title: {detail.title}</li>
-                <li>Description: {detail.description}</li> */}
-                <li>Genre: {detail.name}</li>
-            </div>
-        ))}
+                    {details.map((detail) => (
+                        <div key={detail.id}>
+                            <li>Genres: {detail.name}</li>
+                        </div>
+                    ))}
+                </>
+            }
         </>
     );
+
 }
 
 
